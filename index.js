cc38946a3523f9b4d579ae8d3fb82f9a21624031
MrcsFrança <86272462+MrcsFranca@users.noreply.github.com>
2023-01-16 11:00:40 -0300

    Organização do código

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ function carregar() {
   var msg = window.document.getElementById('msg')
   var img = window.document.getElementById('imagem')
   var data = new Date()
-  //var hora = data.getHours()
-  var hora = 1
+  var hora = data.getHours()
   msg.innerHTML = `Agora são ${hora} horas.`
   if (hora >= 0 && hora < 12) {
     //Bom dia!
-    img.src = 'manha1.png'
+    img.src = 'assets/manha1.png'
     window.document.body.style.background = '#e2cd9f'
   } else if (hora >= 12 && hora < 18) {
     //Boa tarde!
-    img.src = 'tarde1.png'
+    img.src = 'assets/tarde1.png'
     window.document.body.style.background = '#b9846f'
   } else {
     //Boa noite!
-    img.src = 'noite1.png'
+    img.src = 'assets/noite1.png'
     window.document.body.style.background = '#4b85b2'
   }
 }
